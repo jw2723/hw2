559eba4b2f890b97aff9a663e3d411e61e355403
Jingqi Wang <jwang0411@gmail.com>
2024-02-20 23:42:58 -0500

    initialize server

--- data_process.py
+++ data_process.py
@@ -6,39 +6,38 @@ import re
 #     return cleaned_words
 
 
-# Read the Chinese words file
+# read the Chinese txt file
 with open('/Users/jingqiwang/Desktop/In Progress/Cornell University/INFO 5311 visualization/hw2/most-common-words-by-language-master/src/resources/chinese.txt', 'r', encoding='utf-8') as file:
     chinese_words = file.read().splitlines()
 
-# Read the Jap words file
+# read the japanese txt file
 with open('/Users/jingqiwang/Desktop/In Progress/Cornell University/INFO 5311 visualization/hw2/most-common-words-by-language-master/src/resources/japanese.txt', 'r', encoding='utf-8') as file:
     japanese_words = file.read().splitlines()
 
 def clean_chinese_words(words_list):
-    # Regular expression to match only Chinese characters
+    # pattern to match only Chinese characters
     pattern = re.compile(r'[\u4e00-\u9fff]+')
     
-    # Filtering out words that don't match the pattern
+    # filter out words that don't match the pattern
     cleaned_words = [word for word in words_list if pattern.fullmatch(word)]
     
     return cleaned_words
 
 
 def clean_japanese_words(words_list):
-    # Regular expression to match Japanese characters (Hiragana, Katakana, Kanji)
-    # Hiragana range: 3040-309F, Katakana range: 30A0-30FF, Kanji (Common & Uncommon): 4E00-9FAF
+    # pattern to match only Japanese characters (Hiragana, Katakana, Kanji)
     pattern = re.compile(r'[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FAF]+')
     
-    # Filtering out words that don't match the pattern
+    # filter out words that don't match the pattern
     cleaned_words = [word for word in words_list if pattern.fullmatch(word)]
     
     return cleaned_words
 
 
-# Clean the Chinese words list
+# clean the Chinese words list
 cleaned_chinese_words = clean_chinese_words(chinese_words)
 
-# Clean the jap words list
+# clean the japanese words list
 cleaned_japanese_words = clean_japanese_words(japanese_words)
 
 
@@ -46,13 +45,13 @@ print(cleaned_chinese_words)
 print(cleaned_japanese_words)
 
 
-# Optionally, write the cleaned words to a new file
+# write the cleaned words to a new file
 with open('/Users/jingqiwang/Desktop/In Progress/Cornell University/INFO 5311 visualization/hw2/most-common-words-by-language-master/src/resources/cleaned_chinese.txt', 'w', encoding='utf-8') as file:
     for word in cleaned_chinese_words:
         file.write(f"{word}\n")
 
 
-# Optionally, write the cleaned words to a new file
+# write the cleaned words to a new file
 with open('/Users/jingqiwang/Desktop/In Progress/Cornell University/INFO 5311 visualization/hw2/most-common-words-by-language-master/src/resources/cleaned_japanese.txt', 'w', encoding='utf-8') as file:
     for word in cleaned_japanese_words:
         file.write(f"{word}\n")
--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// Dimensions for the SVG container
-const width = 1377;
+// common words word clouds
+
+// SVG container dimensions
+const width = 1477;
 const height = 777;
 
-// Append SVG to the container div
+// append svg to word-cloud div
 const svg = d3.select('#word-cloud')
               .append('svg')
               .attr('width', width)
@@ -10,9 +12,9 @@ const svg = d3.select('#word-cloud')
               .append('g')
               .attr('transform', `translate(${width / 2}, ${height / 2})`);
 
-
+// languages used
 const languages = [
-    { name: 'Chinese', size: 188 },
+    { name: 'Chinese', size: 188 }, // size for initial page
     { name: 'English', size: 188 },
     { name: 'French', size: 158 },
     { name: 'Italian', size: 158 },
@@ -21,54 +23,47 @@ const languages = [
     { name: 'Norwegian', size: 88 }
 ];
 
-// Example color scales (you can add more or choose different ones)
+// color scales for word clouds
 const colorScales = [d3.interpolateRdPu, d3.interpolatePuRd,d3.interpolateYlOrRd,
                      d3.interpolateBuGn, d3.interpolateBuPu,d3.interpolateOrRd, 
                      d3.interpolatePuBu,d3.interpolateYlGn, d3.interpolateYlOrBr];
 
 
-// Function to show flags
+
+// fcns to show & hide floating flags
 function showFlags() {
     const flags = document.querySelectorAll('.flag');
     flags.forEach(flag => flag.classList.remove('hidden'));
 }
-
-// Function to hide flags
 function hideFlags() {
     const flags = document.querySelectorAll('.flag');
     flags.forEach(flag => flag.classList.add('hidden'));
 }
 
-// Function to create and start the initial word cloud layout
+
+// create and display the initial word cloud layout
 function createInitialLayout() {
     console.log("Creating initial layout...");
-
-    // Show flags when going back to the initial layout
-    showFlags();
-    
-    // Set the title and "Go Back" button visibility
     document.getElementById('page-title').style.display = 'block';
     document.getElementById('go-back').style.display = 'none';
+    showFlags(); // show flags after going back to initial layout
 
-    // Clear any existing SVG elements
-    svg.selectAll("*").remove();
+    svg.selectAll("*").remove(); // clear any existing SVG elements
 
-    // Define the layout for the initial word cloud
+    // define layout for initial word cloud
     const initialLayout = d3.layout.cloud()
         .size([width, height])
         .words(languages.map(d => ({ text: d.name, size: d.size })))
         .padding(5)
         .rotate(() => 0)
         .fontSize(d => d.size)
-        .on('end', drawInitial);
+        .on('end', drawInitial); // call once layout computed
 
-    // Start the word cloud layout
-    initialLayout.start();
+    initialLayout.start(); // start word cloud layout
 }
 
 
-
-// Function to render the initial word cloud
+// fcn to render initial word cloud
 function drawInitial(words) {
     console.log("Drawing initial words", words);
 
@@ -77,11 +72,13 @@ function drawInitial(words) {
                     .enter().append('text');
 
     wordEnter.style('font-size', d => `${d.size}px`)
-             .style('fill', 'lightgrey')
+             .style('fill', 'lightgrey') // when unhighlighted
              .attr('text-anchor', 'middle')
              .attr('transform', d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
              .text(d => d.text)
-             .on('mouseover', function (d) {
+
+             // events
+             .on('mouseover', function (d) { // highlight in blc when mouse hover
                  d3.select(this).style('fill', 'black');
              })
              .on('mouseout', function (d) {
@@ -89,55 +86,54 @@ function drawInitial(words) {
              })
              .on('click', function (event, d) {
                  console.log("Language clicked", d);
-                 createWordCloud(d.text);
+                 createWordCloud(d.text); // createWordCloud when lang clicked
              });
 }
 
-// Function to render the language-specific word cloud
+
+// fcn to render language-specific word cloud
 function drawLanguageSpecific(words) {
-    document.getElementById('page-title').style.display = 'none'; // Hide the title
-    document.getElementById('go-back').style.display = 'block'; // Show the go-back button
+    document.getElementById('page-title').style.display = 'none'; // hide title
+    document.getElementById('go-back').style.display = 'block'; // go back button
 
-    // Randomly choose a color scale
     const colorScale = d3.scaleSequential(colorScales[Math.floor(Math.random() * colorScales.length)])
-                        .domain([0, Math.max(...words.map(d => d.size))]);
-
+                        .domain([0, Math.max(...words.map(d => d.size))]); // randomly choose a color scale for wordclouds
 
     svg.selectAll('text')
         .data(words)
         .enter().append('text')
         .style('font-size', d => `${d.size}px`)
-        .style('fill', d => colorScale(d.size)) // Apply color based on size
+        .style('fill', d => colorScale(d.size)) // apply color based on size
         .attr('text-anchor', 'middle')
         .attr('transform', d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
         .text(d => d.text);
 }
 
 
-// Function to load a text file and create a word cloud
+// fcn to load txt files and create a word cloud
 function createWordCloud(language) {
     let filename = '';
-    if (language === 'Chinese' || language === 'Japanese') {
+    if (language === 'Chinese' || language === 'Japanese') { // b/c chn and jap txt are cleaned
         filename = `most-common-words-by-language-master/src/resources/cleaned_${language.toLowerCase()}.txt`;
     } else {
         filename = `most-common-words-by-language-master/src/resources/${language.toLowerCase()}.txt`;
     }
 
-    fetch(filename)
+    fetch(filename) // fetch
         .then(response => response.text())
         .then(data => {
-            svg.selectAll("*").remove();
+            svg.selectAll("*").remove(); // clear existing SVG elements
 
-            // Calculate the word sizes based on rank with explicit size tiers
+            // determine word sizes based on rank in txt
             let words = data.split("\n");
             let sizeScale = i => {
-                if (i < 3) return 180; // Top 10 words
-                if (i < 5) return 150; // Top 10 words
-                if (i < 10) return 120; // Top 10 words
-                if (i < 20) return 100; // Top 10 words
-                if (i < 50) return 80; // Next 10 words
+                if (i < 3) return 180; // top in txt == most common
+                if (i < 5) return 150;
+                if (i < 10) return 120;
+                if (i < 20) return 100;
+                if (i < 50) return 80;
                 if (i < 80) return 50;
-                return 20;  // All other words
+                return 20;  // all other words
             };            
             words = words.map((d, i) => ({ text: d, size: sizeScale(i) }));
 
@@ -145,16 +141,15 @@ function createWordCloud(language) {
             d3.layout.cloud()
                 .size([width, height])
                 .words(words)
-                .padding(5)
+                .padding(5) // btn words
                 .rotate(() => 0)
-                .fontSize(d => d.size)
-                .on('end', drawLanguageSpecific)
-                .start();
+                .fontSize(d => d.size) // font size each word
+                .on('end', drawLanguageSpecific) // once layout computed
+                .start(); // start layout computation
         });
 
-    // Hide flags when a language is selected
-    hideFlags();
+    hideFlags(); // hide flags when lang selected(wordcloud creation)
 }
 
-// Initialize the initial word cloud layout
+// initialize initial word cloud
 createInitialLayout();
